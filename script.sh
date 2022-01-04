b7a79204120753321c652e283e1033a39f115717
Josué Lobo <80691766+RoyMusthang@users.noreply.github.com>
2022-01-03 23:59:51 -0300

    arrumando bug do email ssh

--- script.sh
+++ script.sh
@@ -129,7 +129,7 @@ if [ "$shh" == "s" ] || [ "$shh" == "S" ]; then
   read -p "==== Qual seu email? ====" EMAIL
   read -p "==== Qual seu nome? ====" NAME
   git config --global user.name $NAME
-  git config --global user.name $EMAIL
+  git config --global user.email $EMAIL
   ssh-keygen -t rsa -b 4096 -C $EMAIL
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
